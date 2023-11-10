62f812291903b18c5749ec6ebf10c19b1b988e36
amirmtolouie@gmail.com <amirmtolouie@gmail.com>
2023-11-10 21:49:01 +0300

    Add project files.

---
+++ Array/Array.cs
@@ -0,0 +1,66 @@
+﻿
+namespace DataStructures.Array;
+
+public class Array
+{
+    private int[] numbers;
+    private int count = 0;
+    public Array(int length)
+    {
+        numbers = new int[length];
+    }
+
+    public void insert(int value)
+    {
+
+        if (numbers.Length == count)
+        {
+            int[] newNumbers = new int[count + 1];
+            for (int i = 0; i < count; i++)
+            {
+                newNumbers[i] = numbers[i];
+            }
+            numbers = newNumbers;
+
+        }
+
+        numbers[count++] = value;
+
+    }
+
+    public void removeAt(int index)
+    {
+        if (index < 0 || index >= count)
+            throw new ArgumentException("Index out of range");
+
+
+        for (int i = index; i < count; i++)
+        {
+            if (index == count - 1) // Removing the last element
+            {
+                continue;
+            }
+            numbers[i] = numbers[i + 1];
+        }
+        count--;
+
+    }
+
+    public int indexOf(int value)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            if (numbers[i] == value)
+                return i;
+        }
+        return -1;
+    }
+
+    public void print()
+    {
+        for (int i = 0; i < count; i++)
+        {
+            Console.WriteLine(numbers[i]);
+        }
+    }
+}
---
+++ Array/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+DataStructures.Array.Array numbers = new DataStructures.Array.Array(3);
+
+numbers.insert(10);
+numbers.insert(20);
+numbers.insert(30);
+numbers.insert(40);
+numbers.print();
+
+
+var result = numbers.indexOf(10);
+Console.WriteLine(result);
+
+
+Console.ReadKey();
